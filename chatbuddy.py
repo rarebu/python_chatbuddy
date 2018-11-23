345ff35edbc89b482f8b918753981abccb046c26
rarebu <rafthebee@gmail.com>
2018-11-23 19:37:10 +0100

    testing globalsock in groupmsg

--- chatbuddy.py
+++ chatbuddy.py
@@ -6,14 +6,9 @@ from multiprocessing import Process
 from contextlib import closing
 
 def handle_new_buddy_with_buddylist(buddyname, addr):
-    #if isinstance(addr, tuple):
-        #print("isinstance")
     if type(addr) ==  tuple:
-        #print("istype")
         addr = addr[0]
-    #print("MSG_DATA: " + buddyname + " ----- ADDR: " + str(addr) + " ----- BUDDYLIST: " + str(buddylist))
     if (buddyname, addr) not in buddylist:
-        #print("\nAdding to Buddylist_neeverreachthis: " + str(buddyname))
         print("New Buddy found: " + buddyname)
         buddylist.append((buddyname, addr))
 
@@ -36,22 +31,6 @@ def check_message(msg, addr):
     except IndexError:
         print("empty")
     if msg_prefix == "buddyTCP":
-        #print("IF PREFIX BUDDYTCP_CASE")
-        #print("Message, addr: " + str(msg) + "-----" + str(addr))
-        #re_msg = ("buddyTCP-" + myname).encode('utf-8')
-        #shortsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #print("addr: "+ str(addr))
-        #try:
-            #shortsock.connect(addr)
-        #except TypeError:
-            #shortsock.connect((addr, 50000))
-        #try:
-            #shortsock.send(re_msg)
-        #except ConnectionResetError:
-        #    print("connection reset error")
-         #   shortsock.close()
-        #    return ConnectionResetError
-        #shortsock.close()
         handle_new_buddy_with_buddylist(msg_data, addr)
         return msg_data
     elif msg_prefix == "buddyMSG":
@@ -68,7 +47,6 @@ def check_message(msg, addr):
         except ValueError:
             print("empty")
 
-
 def handle_found_host(address = None):
     ssock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     conn = ssock.connect((address, 50000))
@@ -76,9 +54,8 @@ def handle_found_host(address = None):
     conn = ssock.send(msg.encode('utf-8'))
     while True:
         try:
-            #print("recving message in handle_Found_host")
-            msg = ssock.recv(1024).decode('utf-8') #CHANGE TO conn.recv
-            if not msg:  # receiving empty messages means that the socket other side closed the socket
+            msg = ssock.recv(1024).decode('utf-8')
+            if not msg:
                 print("connection closed!!!")
                 ssock.close()
                 break
@@ -97,22 +74,17 @@ def handle_found_host(address = None):
         print("empty")
 
 def port_scan(host):
-    #print("scanning " + host)
     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
         sock.settimeout(.1)
         conn = sock.connect_ex((host, 50000))
         sock.close()
         if conn == 0:
             if host != mylocalip:
-                #print("\nHost ", host, " is open")
                 newbuddy_thread = threading.Thread(target=handle_found_host, kwargs= {"address": host})
                 newbuddy_thread.daemon = True
                 newbuddy_thread.start()
 
 def search_partners():
-    #first = ipaddress.IPv4Address("192.168.0.34")
-    #last = ipaddress.IPv4Address("192.168.0.100")
-    #ip_range = ipaddress.summarize_address_range(first, last)
     count = 0
     for ip in range(1, 256):
         count += 1
@@ -120,19 +92,16 @@ def search_partners():
     print("Scanned " + str(count) +" Hosts")
 
 def handle_incoming_connection(conn, addr):
-    nameofconnectedbuddy = ""
-    #print('\nIncoming connection accepted: ', addr)
     id = ("buddyTCP-" + myname).encode('utf-8')
     conn.send(id)
     while True:
         try:
             data = conn.recv(1024)
-            if not data:  # receiving empty messages means that the socket other side closed the socket
+            if not data:
                 time.sleep(0.5)
                 continue
             msg = data.decode('utf-8')
             try:
-                #print("Checking message in IDLER")
                 msg_buddyname = check_message(msg, addr)
             except IndexError:
                 break
@@ -143,14 +112,12 @@ def handle_incoming_connection(conn, addr):
             conn.close()
             break
         except OSError:
-            #print("Connection (closed) because of OSError")
             continue
     try:
         buddylist.remove((msg_buddyname, addr))
         print("Buddy " + msg_buddyname + "disconnected")
     except UnboundLocalError:
         print("empty")
-
 
 def tcp_server():
     sock.listen(1)
@@ -165,7 +132,6 @@ def tcp_server():
             p.start()
         except socket.timeout:
             print('\nSocket timed out listening', time.asctime())
-
     sock.close()
 
 def printlist():
@@ -179,13 +145,10 @@ def chat():
     selection=input("\nPlease choose the Number of your ChatBuddy: ")
     data=input("\nEnter your Message: ")
     msg = ("buddyMSG-" + myname + "-" + data).encode('utf-8')
-
     x = buddylist[int(float(selection))]
     buddy_addr = x[1]
     ssock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #print("\n(buddy addr: " + str(buddy_addr))
     conn = ssock.connect((buddy_addr, 50000))
-
     try:
         ssock.send(msg)
     except ConnectionResetError:
@@ -210,13 +173,13 @@ def group_chat():
 def send_quit_msg():
     msg = ("buddyQUIT-" + myname).encode('utf-8')
     for buddy in buddylist:
-        ssssock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        conn = ssssock.connect((buddy[1], 50000))
+        #tmpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #conn = tmpsock.connect((buddy[1], 50000))
         try:
-            ssssock.send(msg)
+            sock.send(msg)
         except ConnectionResetError:
             print("connection reset error")
-            conn.close()
+            #conn.close()
             return ConnectionResetError
 
 def main_menu():
@@ -236,11 +199,9 @@ def main_menu():
     myname = input('Enter your Nickname: ')
     global buddylist
     buddylist = []
-
     server_thread = threading.Thread(target=tcp_server)
     server_thread.daemon = True
     server_thread.start()
-
     while True:
         try:
             choice = input('choose an option ')
@@ -264,7 +225,6 @@ def main_menu():
             sys.exit()
         else:
             print('Valid options are S (Scan), L (List), C (Chat), G (GroupChat), Q (Quit)')
-
 
 if __name__ == '__main__':
     main_menu()
